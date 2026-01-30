d4b9be2db46f036f504e70e05c1ab822d344af56
Ganbayar19 <ganbayargantuya9@gmail.com>
2026-01-30 16:19:31 +0800

    Remove comments from win conditions in checkWin function

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function checkWin() {
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8], // rows
+    [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8], // columns
+    [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6], // diagonals
+    [2, 4, 6],
   ];
 
   for (const condition of winConditions) {
